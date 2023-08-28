b8c656e56b3040b880d806abca89e8aab76327ec
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-08-28 22:43:12 +0600

    tasks are added dynamically from the user

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,9 +3,13 @@ import MyTasks from '../components/tasks/MyTasks';
 import TaskCard from '../components/tasks/TaskCard';
 import { useState } from 'react';
 import AddTaskModal from '../components/tasks/AddTaskModal';
+import { useSelector } from 'react-redux';
 
 const Tasks = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { tasks } = useSelector((state) => state.tasksSlice);
+
+
 
   return (
     <div className="h-screen grid grid-cols-12">
@@ -24,7 +28,7 @@ const Tasks = () => {
             <button
               onClick={() => setIsOpen(!isOpen)}
               className="btn btn-primary">Add Task</button>
-            <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen}/>
+            <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen} />
 
             <div className="h-10 w-10 rounded-xl overflow-hidden">
               <img
@@ -44,7 +48,11 @@ const Tasks = () => {
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id}
+                  task={item}
+                />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
@@ -55,19 +63,26 @@ const Tasks = () => {
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id}
+                  task={item}
+                />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>Up Next</h1>
+              <h1>Complete</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
                 0
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id}
+                  task={item}
+                />
+              ))}
             </div>
           </div>
         </div>
