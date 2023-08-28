071e7de1bf455463fcca83c0b9b53e3b5bd2bfa3
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-08-29 00:25:16 +0600

    remove tasks

---
+++ src/redux/features/user/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState ={
+    name: 'Mir Hussain',
+    email: 'mir@gamil.com',
+    userTasks: [],
+};
+
+const userSlice = createSlice({
+    name: 'userSlice',
+    initialState,
+    reducers: {
+
+    },
+});
+
+export default userSlice.reducer;
